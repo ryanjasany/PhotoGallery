59230fdbf1ad2e0ca0e61255205a6b28f91515f5
Ryan <ryanjasany6@gmail.com>
2017-11-27 20:25:06 -0500

    initial commit

---
+++ app/src/main/java/com/example/ryan/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.example.ryan.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ryan on 10/11/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity{
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+
+        }
+
+
+    }
+
+}

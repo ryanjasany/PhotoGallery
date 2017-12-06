1828cf67d07f45c437e46a08a3cec3ad9711c197
Ryan <ryanjasany6@gmail.com>
2017-12-05 19:25:51 -0500

    Chapter 28 polling

--- app/src/main/java/com/example/ryan/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/ryan/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.ryan.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -139,6 +140,15 @@ public class PhotoGalleryFragment extends Fragment{
 
         });
 
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if(PollService.isServiceAlarmOn(getActivity())){
+            toggleItem.setTitle(R.string.stop_polling);
+
+
+        }else{
+            toggleItem.setTitle(R.string.start_polling);
+        }
+
 
     }
 
@@ -149,6 +159,11 @@ public class PhotoGalleryFragment extends Fragment{
             case R.id.menu_item_clear:
                 QueryPreferences.setStoredQuery(getActivity(), null);
                 updateItems();
+                return true;
+            case R.id.menu_item_toggle_polling:
+                boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+                PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+                getActivity().invalidateOptionsMenu();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
